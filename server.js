ac785799bae9cb807c27ed09c2dca805717a2820
unknown <vijaysjoshi2003@gmail.com>
2025-03-19 00:37:27 +0530

    vijays ninja server

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Node.js Server!');
+    res.send('CONNECTED TO VIJAYS NINJA SERVER');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://192.168.29.66:${PORT}`);
 });
